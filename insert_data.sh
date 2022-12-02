6fc39920c3fcf5f9ffe31459fce8aadea82ded25
JoshuaTaljard <115033832+JoshuaTaljard@users.noreply.github.com>
2022-12-02 10:35:01 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -8,8 +8,25 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+#delete teams and games tables
 echo $($PSQL "TRUNCATE teams, games")
 
+
+# read data from tables, write into games table
+cat games.csv | while IFS="," read YEAR ROUND THIS_WINNER THIS_OPPONENT W_GOALS O_GOALS
+do
+
+  #ignore header of csv file
+  if [[ $YEAR != "year" ]]
+  then
+    OPPONENT=$($PSQL "SELECT team_id FROM teams WHERE teams.name='$THIS_OPPONENT'")
+    WINNER=$($PSQL "SELECT team_id FROM teams WHERE teams.name='$THIS_WINNER'")
+    INSERT_DATA_RESULT=$($PSQL " INSERT INTO games(year,round,winner_id,opponent_id, opponent_goals,winner_goals) VALUES($YEAR, '$ROUND', $WINNER, $OPPONENT, $O_GOALS, $W_GOALS)")
+  fi
+done
+
+#read data from games file, populate unique values into TEAMS table
 cat games.csv| while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
   #ignore csv file header
@@ -23,8 +40,7 @@ do
     if [[ -z $OPPONENT_ID ]]
     then
       OPPONENT_INSERT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-    fi  
-    
+    fi      
     #no matching winner in database - insert new winner
     if [[ -z $WINNER_ID ]]
     then
@@ -33,19 +49,4 @@ do
   fi
 done
 
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do
-  # remove header
 
-  if [[ $YEAR != "year" ]]
-  then
-  WIN_ID=$($PSQL "SELECT team_id FROM teams WHERE teams.name='$WINNER'")
-  OPP_ID=$($PSQL "SELECT team_id FROM teams WHERE teams.name='$OPPONENT'")
-  # insert row and show process ok
-  INSERT_DATA_RESULT=$($PSQL " INSERT INTO games(year,round,winner_id,opponent_id,winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WIN_ID, $OPP_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
-    if [[ $INSERT_DATA_RESULT == "INSERT 0 1" ]]
-    then
-    echo inserted data $INCREMENT : $YEAR -- $ROUND -- $OPPONENT -- Score: $WINNER_GOALS - $OPPONENT_GOALS
-    fi
-  fi
-done
